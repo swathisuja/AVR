b66019c9a69b72fd5c830bcd402705e1540c6432
swathi.moorthy@microchip.com <swathi.moorthy@microchip.com>
2021-03-04 08:32:34 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mega324pb.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/mega324pb.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mega324pb.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mega324pb.tar
